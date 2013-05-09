15dc61c7b86bb9324660955d5c52abb28c595554
Vladimir Markovnin <markovnin@gmail.com>
2013-05-09 20:46:19 +0400

    Основной функционал реализован

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,6 +10,7 @@ class QuizzesController < ApplicationController
   # GET /quizzes/1
   # GET /quizzes/1.json
   def show
+    send_data @quiz.data, :filename => @quiz.name, :type => 'application/pdf', :disposition => "inline"
   end
 
   # GET /quizzes/new
@@ -26,15 +27,65 @@ class QuizzesController < ApplicationController
   # POST /quizzes.json
   def create
     @quiz = Quiz.new(name: quiz_params[:name])
-    @subjects = Hash.new
-    params.each do |key, value|
-      @subjects[key] = value if key.to_i.is_a? Fixnum
+    @subjects = Subject.all
+    @counts = Hash.new
+    @subjects.each do |subject|
+      @counts[subject.id] = params[subject.id.to_s].to_i if params[subject.id.to_s].to_i > 0
     end
-    @quiz.data = @subjects.to_s
+    @questions = Array.new
+    @answers = Hash.new
+    @counts.each do |key, value|
+      @questions << Subject.find(key).questions.first(value)
+    end
+    pdf = Prawn::Document.new(page_size: "A4", :info => {
+:Title => @quiz.name,
+:Author => "Vladimir Markovnin",
+:Subject => "IPO ISMA",
+:Creator => "ISMA",
+:Producer => "Prawn",
+:CreationDate => Time.now }
+)
+    pdf.font_families.update("Ubuntu" => {
+      :normal => "#{Rails.root}/vendor/fonts/Ubuntu-R.ttf",
+      :italic => "#{Rails.root}/vendor/fonts/Ubuntu-RI.ttf",
+      :bold => "#{Rails.root}/vendor/fonts/Ubuntu-B.ttf"
+                                         })
+    pdf.font "Ubuntu"
+    pdf.text @quiz.name, :style => :bold, :size => 18
+    pdf.move_down 10
+    n = 0
+    @questions.each do |question|
+      pdf.text question[0].subject.name, :style => :bold, :size => 16
+      pdf.move_down 10
+      question.shuffle.each do |q|
+	n += 1
+	pdf.text n.to_s + ". " + q.text, :style => :italic
+	pdf.move_down 10
+	m = 0
+	q.answers.shuffle.each do |answer|
+	  m += 1
+	  @answers[n] = m if answer.right?
+	  pdf.text m.to_s + ". " + answer.text, :indent_paragraphs => 15
+	  pdf.move_down 10
+	end
+      end
+    end
+    string = "page <page> of <total>"
+    options = {:at => [pdf.bounds.right - 150, 0], :width => 150, :align => :center, :start_count_at => 1}
+    pdf.number_pages string, options
+
+    pdf.start_new_page
+    pdf.text t(:answers, :scope => :quiz), :style => :bold, :size => 18
+    pdf.column_box([0, pdf.cursor], :columns => 2, :width => pdf.bounds.width) do
+      @answers.each do |key, value|
+	  pdf.text t(:question, :scope => :quiz)+ ": " + key.to_s + ' - ' + value.to_s
+      end
+    end
+    @quiz.data = pdf.render
 
     respond_to do |format|
       if @quiz.save
-        format.html { redirect_to @quiz, notice: 'Quiz was successfully created.' }
+        format.html { redirect_to quizzes_path, notice: 'Quiz was successfully created.' }
         format.json { render action: 'show', status: :created, location: @quiz }
       else
         format.html { render action: 'new' }
@@ -66,7 +117,7 @@ class QuizzesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_quiz
